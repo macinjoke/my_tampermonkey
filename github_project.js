e15aadad674127454effc496cbc2447ceeb2ccdc
Shunji Makino <shunji.makino@gmail.com>
2017-10-30 20:47:16 +0900

    add comment for tampermonkey

--- github_project.js
+++ github_project.js
@@ -1,8 +1,17 @@
-// add New Issue button in project page
+// ==UserScript==
+// @name         github project
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add new issue button in project page
+// @author       makky
+// @match        https://github.com/*/*/projects/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// ==/UserScript==
 
 (function() {
     'use strict';
-    console.log('New Button was added');
+    console.log('New Issue Button was added');
     var author = $('span.author > a').eq(0).text();
     var repository = $('span.author > span > a').text();
     $('.d-flex.flex-justify-between.flex-items-center.f6').after('<a href="https://github.com/' + author + '/' + repository + '/issues/new">New Issue</a>');
